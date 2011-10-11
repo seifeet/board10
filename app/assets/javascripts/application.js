c039153e326694d04ccf2dbc70b9dee0d6846ce1
AT <at@seifeet.com>
2011-10-11 12:21:10 -0700

    css for signin and signup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -146,3 +146,30 @@ the following conditions:
     });
 
 })(jQuery);
+/**
+* Image background
+**/
+$(window).load(function() {    
+
+        var theWindow        = $(window),
+            $bg              = $("#bg"),
+            aspectRatio      = $bg.width() / $bg.height();
+
+        function resizeBg() {
+                if ( (theWindow.width() / theWindow.height()) < aspectRatio ) {
+                    $bg
+                        .removeClass()
+                        .addClass('bgheight');
+                } else {
+                    $bg
+                        .removeClass()
+                        .addClass('bgwidth');
+                }
+
+        }
+
+        theWindow.resize(function() {
+                resizeBg();
+        }).trigger("resize");
+
+});
